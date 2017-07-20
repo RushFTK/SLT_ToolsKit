cb3cdfe0c473a83118cc78ad848f32238791c863
RushFTK <867871766@qq.com>
2017-07-20 14:06:28 +0800

    update a shellscript which can automatic download project into local

---
+++ dl_project.sh
@@ -0,0 +1,11 @@
+# upgrade system
+sudo apt-get update && sudo apt-get install
+# upgrade git in Ubuntu/Debian
+sudo apt-get install git
+# upgrade git in RedHat/Fedora (delete '#' in the next line to use it)
+# yum install git && dnf install git
+# go to home folder
+cd ~
+# download project
+git clone https://github.com/RushFTK/SLT_ToolsKit.git
+
